e082583855d329a00436af2ddc3b42ebbf2a7884
azare242 <azare242@gmail.com>
2021-02-09 18:50:11 +0330

    BOT functions added

--- main.c
+++ main.c
@@ -13,7 +13,7 @@
 #include <string.h>
 #include "maps.h"
 #include "gameloop.h"
-
+#include <time.h>
 void start() {
     printf("*********************************\n");
     printf("*     WELCOME TO BATTLESHIP     *\n");
@@ -46,12 +46,18 @@ User * choseuser(User * head);
 void initshowmap_empty(char map[map_rows][map_columns]);
 void initplayermap_empty(int map[map_rows][map_columns]);
 void printmap_for_set(int map[map_rows][map_columns]);
-void printmap_ingame(char map[map_rows][map_columns]);
+void printmap(char map[map_rows][map_columns]);
 void newship(Ships ** head,int xb,int yb,int xe,int ye,int size,int state);
 bool isnotshiphere(int xb,int xe,int yb,int ye,int state,int map[map_rows][map_columns]);
 void getships(int map[map_rows][map_columns],Ships * head,int size);
 void getshipsize1(int map[map_rows][map_columns],Ships * head);
 void setmap(int map[map_rows][map_columns],Ships * head);
+int random_number1_2();
+int random_number0_9();
+void bot_give_ships(int map[map_rows][map_columns],Ships * botships,int size);
+void bot_give_ships_size1(int map[map_rows][map_columns],Ships * botships);
+void bot_set_map(int map[map_rows][map_columns],Ships * botships);
+void bot_attack(char showmap[map_rows][map_columns],int map_player[map_rows][map_columns]);
 
 int main() {
     system("color 10");
@@ -591,6 +597,121 @@ void play_with_friend(User * head)
     setmap(map_player2,player2_ships);
 
 
+}
+int random_number1_2()
+{
+    srand(time(NULL));
+    return ((rand() % 2) + 1);
+}
+int random_number0_9()
+{
+    srand(time(NULL));
+    return rand() % 10;
+}
+void bot_give_ships(int map[map_rows][map_columns],Ships * botships,int size)
+{
+    int xb,xe,yb,ye,state;
+    state = random_number1_2();
+
+    if (state == 1){
+        while (1){
+            xb = random_number0_9();
+            if (xb + size > 9) xb -= size ;
+            xe = xb;
+            yb = random_number0_9();
+            if (yb + size > 9) yb -= size;
+            ye =  yb + size - 1 ;
+            if(xe == xb && ye - yb + 1 == size && ye > yb && isnotshiphere(xb,xe,yb,ye,state,map) && ye < 10 && yb < 9 - size){
+                break;
+            }
+            else continue;
+        }
+    }
+    else if (state == 2) {
+        while (1) {
+            xb = random_number0_9();
+            if (xb + size > 9) xb -= size;
+            xe = xb + size - 1;
+            yb = random_number0_9();
+            if (yb + size > 9) yb -= size;
+            ye = yb;
+            if (ye == yb && xe - xb + 1 == size && xe > xb && isnotshiphere(xb, xe, yb, ye, state, map) && xe < 10 && xb < 9 - size) {
+                break;
+            }
+            else continue;
+        }
+
+    }
+    setshipsonmap(xb,xe,yb,ye,state,map);
+    newship(&botships,xb,yb,xe,ye,size,state);
+    botships = botships->next;
+}
+void bot_give_ships_size1(int map[map_rows][map_columns],Ships * botships)
+{
+    int x,y;
+    while (1) {
+        x = random_number0_9();
+        y = random_number0_9();
+        if (isnotshiphere(x, x, y, y, 1, map)) break;
+        else continue;
+    }
+    setshipsonmap(x,x,y,y,1,map);
+    newship(&botships,x,y,x,y,1,1);
+    botships = botships->next;
+}
+void bot_set_map(int map[map_rows][map_columns],Ships * botships)
+{
+    printf("Please wait , Bot is Setting MAP\n");
+    setshipsonmap(0,0,8,9,1,map);
+    newship(&botships,0,8,0,9,1,1);
+    setshipsonmap(2,3,9,9,2,map);
+    newship(&botships,2,9,3,9,2,2);
+    bot_give_ships(map,botships,5);
+    bot_give_ships(map,botships,3);
+    bot_give_ships(map,botships,3);
+    bot_give_ships(map,botships,2);
+    bot_give_ships_size1(map,botships);
+    bot_give_ships_size1(map,botships);
+    bot_give_ships_size1(map,botships);
+    bot_give_ships_size1(map,botships);
+    //printmap_for_set(map);
+    printf("The map of BOT sat\n");
+    getchar();
+    getchar();
+    system("cls");
+
+}
+void bot_attack(char showmap[map_rows][map_columns],int map_player[map_rows][map_columns])
+{
+    int attac_x,attac_y;
+    while (1)
+    {
+        attac_x = random_number0_9();
+        attac_y = random_number0_9();
+        if ( (map_player[attac_x][attac_y] == 0 || map_player[attac_x][attac_y] == 2) && showmap[attac_x][attac_y] == '-'){
+            printf("BOT Shot went wrong!!!\n");
+            showmap[attac_x][attac_y] = 'W';
+            printmap(showmap);
+            printf("\nPress Enter To Continue\n");
+            getchar();
+            getchar();
+            system("cls");
+            break;
+        }else if (map_player[attac_x][attac_y] == 1 && showmap[attac_x][attac_y] == '-'){
+            printf("Oops !!! BOT Hits Your Ships!!!\n");
+            showmap[attac_x][attac_y] = 'E';
+            map_player[attac_x][attac_y] = -1;
+            printmap(showmap);
+            printf("\nPress Enter To Continue\n");
+            getchar();
+            getchar();
+            system("cls");
+        }
+        else if (showmap[attac_x][attac_y] == 'W'){
+            continue;
+
+        }
+    }
 }
 /*
  * With Thank to Saman Husseini ,Muhammad Fatemi, Amirparsa Salmankhah(DADDY) , Faraz Farangi Zadeh , and َ All TAs
